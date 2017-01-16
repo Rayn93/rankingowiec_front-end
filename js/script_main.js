3a8d9fdafc05a34fe39cb1949c07ff9c54456fee
Rob <robert.saternus@gmail.com>
2017-01-16 11:13:42 +0100

    Finish object page + pie chart

--- js/script_main.js
+++ js/script_main.js
@@ -91,3 +91,84 @@ $(document).ready(function () {
 //
 //document.querySelector('meta[name="viewport"]').setAttribute('content', content);
 
+
+
+//PIE CHART ON OBJECT PAGE
+
+var myCanvas = document.getElementById("myCanvas");
+myCanvas.width = 200;
+myCanvas.height = 200;
+
+var ctx = myCanvas.getContext("2d");
+
+function drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color ){
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(centerX,centerY);
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.closePath();
+    ctx.fill();
+}
+
+var Piechart = function(options){
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+
+    this.draw = function(){
+        var total_value = 0;
+        var color_index = 0;
+        for (var categ in this.options.data){
+            var val = this.options.data[categ];
+            total_value += val;
+        }
+
+        var start_angle = 0;
+        for (categ in this.options.data){
+            val = this.options.data[categ];
+            var slice_angle = 2 * Math.PI * val / total_value;
+
+            drawPieSlice(
+                this.ctx,
+                this.canvas.width/2,
+                this.canvas.height/2,
+                Math.min(this.canvas.width/2,this.canvas.height/2),
+                start_angle,
+                start_angle+slice_angle,
+                this.colors[color_index%this.colors.length]
+            );
+
+            start_angle += slice_angle;
+            color_index++;
+        }
+
+        start_angle = 0;
+        for (categ in this.options.data){
+            val = this.options.data[categ];
+            slice_angle = 2 * Math.PI * val / total_value;
+            var pieRadius = Math.min(this.canvas.width/2,this.canvas.height/2);
+            var labelX = this.canvas.width/2 + (pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
+            var labelY = this.canvas.height/2 + (pieRadius / 2) * Math.sin(start_angle + slice_angle/2);
+
+            var labelText = Math.round(100 * val / total_value);
+            this.ctx.fillStyle = "white";
+            this.ctx.font = "bold 20px Arial";
+            this.ctx.fillText(labelText+"%", labelX,labelY);
+            start_angle += slice_angle;
+        }
+    };
+
+
+};
+
+var myPiechart = new Piechart(
+    {
+        canvas:myCanvas,
+        data:rankingData,
+        colors:["#4875ff","#ff1719"]
+    }
+);
+myPiechart.draw();
+
+//END PIE CHART ON OBJECT PAGE
